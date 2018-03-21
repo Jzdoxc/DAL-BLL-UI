cf925f8c7da8c1e44590d263c78e2aecff623e35
Jzdoxc <Jzdoxc@outlook.com>
2018-03-21 10:35:54 +0800

    拼音组件完成!

--- UI/test/PinYinTest.cs
+++ UI/test/PinYinTest.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.International.Converters.PinYinConverter;
+using Common;
 namespace UI.test
 {
     public partial class PinYinTest : Form
@@ -18,15 +18,32 @@ namespace UI.test
         }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            string s = textBox1.Text;
-            foreach (char c in s)
-            {
-                ChineseChar cc = new ChineseChar(c);
-                foreach(var c1 in cc.Pinyins)
-                {
-                    label1.Text += c1 + " ";
-                }
-            }
+            label1.Text = PinYinHelper.GetPinYin(textBox1.Text);
+            ////清空文本字符串
+            //label1.Text = "";
+            ////获取用户输入的字符串
+            //string s = textBox1.Text;
+            ////遍历字符串获取拼音
+            //foreach (char c in s)
+            //{
+            //    //判断指定的字符是否是汉字
+            //    if (ChineseChar.IsValidChar(c))
+            //    {
+            //        //构造字符对象
+            //        ChineseChar cc = new ChineseChar(c);
+            //        //获取拼音首字母，返回集合，因为存在多音字
+            //        label1.Text += cc.Pinyins[0][0];
+            //    }
+
+            //    //foreach(var c1 in cc.Pinyins)
+            //    //{
+            //    //    label1.Text += c1 + " ";
+            //    //}
+            //    else
+            //    {
+            //        label1.Text += c;
+            //    }
+            //}
         }
     }
 }
